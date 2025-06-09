d69d9d5af798ae8d07f103a215256534529f8ae4
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-06-09 12:52:21 +0200

    Add updateLibrary and removeLibrary functions Functions that allow to display our library correctly every time a new book is added

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let libraryDisplayContainer = document.querySelector(".libraryDisplayContainer");
+
 let library = [];
 
 function Book(title, author, pages) {
@@ -11,4 +13,33 @@ function addBookToLibrary(title, author, pages) {
 
     library.push(new Book(title, author, pages));
 
+    updateLibrary();
+
+}
+
+function updateLibrary() {
+
+    removeLibrary();
+
+    library.forEach((book) => {
+        let bookContainer = document.createElement("div");
+        bookContainer.setAttribute("class", "bookContainer");
+
+        const title = document.createElement("h1");
+        title.textContent = book.title;
+        const author = document.createElement("h2");
+        author.textContent = book.author;
+        const pages = document.createElement("h3");
+        pages.textContent = "Pages: " + book.pages;
+
+        bookContainer.append(title, author, pages);
+
+        libraryDisplayContainer.appendChild(bookContainer);
+    });
+
+}
+
+function removeLibrary() {
+    let booksToRemove = document.querySelectorAll(".bookContainer");
+    booksToRemove.forEach((book) => { book.remove() });
 }
